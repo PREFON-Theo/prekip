8cbc8254e2bdd9204e10f37b9f13c8d7f3489d40
PREFON-Theo <theo.racher-raulin@prefon.fr>
2023-06-30 10:48:08 +0200

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const jwtSecret = 'JNaZcAPqBr4dPqiMhwavDjZCgABEQKLJyj6Cq8aJukvoXGHi'
 
 mongoose.set('strictQuery', false);
 mongoose
-    .connect(process.env.MONGO_URL)
+    .connect('mongodb://mongo/prekip')
     .then(() => console.log("Connected"))
     .catch(()=> console.log("Not connected"))
 
@@ -31,7 +31,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL
+    origin: 'http://localhost:3000'
 }))
 
 //////////////////////////////////////////
